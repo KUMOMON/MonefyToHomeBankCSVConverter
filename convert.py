8a7067d02d12fedd1811485e233b054ceb62e5af
Alexander Sobolev <sobolev.alexander666@gmail.com>
2018-12-19 16:32:49 +0300

    fix format and complete script

--- convert.py
+++ convert.py
@@ -3,33 +3,22 @@ import csv
 from datetime import datetime
 
 # csv file name
-filename = 'testfile.csv'
-
+inputFileName = 'testfile.csv'
+outputFileName = 'outfile.csv'
 # initializing the titles and rows list
 # fields = []
-rows = []
-
-fields = [["test","test2"],["test3","test4"]]
-
-with open('some.csv', mode='w',newline='', encoding='utf8') as f:
-    writer = csv.writer(f,delimiter=';')
-    writer.writerow(["date","paymode","info","payee","description","amount","category"])
-    writer.writerows(fields)
-
-"""
-
 # read data from input csv
-with open(filename,mode='r', encoding='utf8') as csvfile:
-    
-    csvreader = csv.reader(csvfile, delimiter=';')
 
-    # extracting field names through first row
-    fields = csvreader.__next__()
+with open(inputFileName, mode='r', encoding='utf8') as csvfile:
+    with open(outputFileName, mode='w',encoding='utf8', newline='') as f:
+        csvreader = csv.reader(csvfile, delimiter=';')
+        writer = csv.writer(f, delimiter=';')
 
-    # extracting each data row one by one
-    for row in csvreader:
-        # tmp = row;
-        rows.append([datetime.strptime(row[0],'%d/%m/%Y').strftime('%d-%m-%Y'),'0',row[7],row[1],"",row[3],row[2]])
-    pass
 
-"""
+        # writer.writerow(["date", "paymode", "info", "payee", "description", "amount", "category"])
+
+        # extracting field names through first row
+        csvreader.__next__()
+        # extracting each data row one by one
+        for row in csvreader:
+            writer.writerow([datetime.strptime(row[0],'%d/%m/%Y').strftime('%d-%m-%Y'),'0',row[7],row[1],"",row[3],row[2],""])
