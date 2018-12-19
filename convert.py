8f9895dae9440f212d9d6959ae033eceb165cec3
Alexander Sobolev <sobolev.alexander666@gmail.com>
2018-12-19 14:04:29 +0300

    fix read lines from file with encoding utf8

--- convert.py
+++ convert.py
@@ -2,7 +2,7 @@ import csv
 
 filename = 'testfile.csv'
 
-with open(filename, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=" ", quotechar=';')
+with open(filename,encoding='utf8', newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter="\n",quotechar=';')
     for row in csvreader:
         print(', '.join(row))
