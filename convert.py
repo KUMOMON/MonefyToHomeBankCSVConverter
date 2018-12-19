9e3b16b913c3440ce9c6ca0bb8289c3edca36c02
Alexander Sobolev <sobolev.alexander666@gmail.com>
2018-12-19 14:51:03 +0300

    simple read data from monefy CSV

--- convert.py
+++ convert.py
@@ -1,8 +1,23 @@
+# importing csv module
 import csv
 
+# csv file name
 filename = 'testfile.csv'
 
-with open(filename,encoding='utf8', newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter="\n",quotechar=';')
+# initializing the titles and rows list
+fields = []
+rows = []
+
+with open(filename, encoding='utf8') as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=';' )
+    """for row in csvreader:
+        print(', '.join(row))"""
+
+    # extracting field names through first row
+    fields = csvreader.__next__()
+
+    # extracting each data row one by one
     for row in csvreader:
-        print(', '.join(row))
+        rows.append(row)
+
+    pass
