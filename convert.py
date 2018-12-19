ca76e41f88ec86c0be2e28d1a92dfd792c4f0eeb
Alexander Sobolev <sobolev.alexander666@gmail.com>
2018-12-19 16:04:17 +0300

    implement write to new format CSV

--- convert.py
+++ convert.py
@@ -6,15 +6,22 @@ from datetime import datetime
 filename = 'testfile.csv'
 
 # initializing the titles and rows list
-fields = []
+# fields = []
 rows = []
 
+fields = [["test","test2"],["test3","test4"]]
+
+with open('some.csv', mode='w',newline='', encoding='utf8') as f:
+    writer = csv.writer(f,delimiter=';')
+    writer.writerow(["date","paymode","info","payee","description","amount","category"])
+    writer.writerows(fields)
+
+"""
 
 # read data from input csv
-with open(filename, encoding='utf8') as csvfile:
+with open(filename,mode='r', encoding='utf8') as csvfile:
+    
     csvreader = csv.reader(csvfile, delimiter=';')
-    """for row in csvreader:
-        print(', '.join(row))"""
 
     # extracting field names through first row
     fields = csvreader.__next__()
@@ -24,3 +31,5 @@ with open(filename, encoding='utf8') as csvfile:
         # tmp = row;
         rows.append([datetime.strptime(row[0],'%d/%m/%Y').strftime('%d-%m-%Y'),'0',row[7],row[1],"",row[3],row[2]])
     pass
+
+"""
